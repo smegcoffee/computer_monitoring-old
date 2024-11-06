fedc8002204fb5a403bc92ae66445b3f7613027d
emi-Meg <smeg3478@gmail.com>
2024-11-06 09:04:02 +0800

    change axios to url and users.jsx change the http to axios

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://desstrongmotors.com/monitoringback",
-  baseURL: "http://136.239.196.178:5001",
+  // baseURL: "http://136.239.196.178:5001",
   // baseURL: "http://10.50.2.220:8000",
   // baseURL: "http://localhost:8000",
   withCredentials: true,
