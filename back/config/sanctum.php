c13b3eb841a68b05ffb06e9d36c5ddc28e2f4bb2
Jenecil Patac <jenecilpatac@gmail.com>
2024-12-12 14:52:20 +0800

    added dockefiles in back

--- back/config/sanctum.php
+++ back/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,10.10.2.194:9001,localhost:9001,https://backend_monitoring.smctgroup.ph',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
