93ba48180ab644dc51b7e42230152567bad1b847
Jenecil Patac <jenecilpatac@gmail.com>
2024-12-24 14:40:26 +0800

    added dockerfiles

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,7 @@ function App() {
             <Route path="/admin/users-list" element={<Users />} />
           </Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </AdminProvider>
   );
 }
